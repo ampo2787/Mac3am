f13946dee67ac48a48695f43ddd8e0d6b90e94b6
JIHOON PARK <ampo2787@cnu.ac.kr>
2019-05-11 16:07:56 +0900

    Initial Commit

---
+++ Mac3am/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mac3am
+//
+//  Created by JihoonPark on 11/05/2019.
+//  Copyright © 2019 Mac3am. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
