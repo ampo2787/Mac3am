32406e7ba08bd074d9648d02ebd6ae13a23661d4
JIHOON PARK <ampo2787@cnu.ac.kr>
2019-05-26 16:38:29 +0900

    ui

--- Mac3am/PlayViewController.swift
+++ Mac3am/PlayViewController.swift
@@ -9,14 +9,52 @@
 import Cocoa
 
 class PlayViewController: NSViewController {
+    
     @IBOutlet weak var botton_View: NSView!
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var userInfoTextField: NSTextField!
+    let tableViewData = ["Ballad", "R&B", "ROCK", "HIPHOP"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.wantsLayer = true
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
     
     override func viewWillAppear() {
         botton_View.layer?.backgroundColor = NSColor.cyan.cgColor
+        self.tableView.reloadData()
+    }
+
+
+}
+
+extension PlayViewController: NSTableViewDataSource, NSTableViewDelegate {
+    
+    fileprivate enum CellIdentifiers {
+        static let NameCell = "playList"
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return (tableViewData.count)
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        var cellIdentifier: String = ""
+        
+        
+        // 2
+        if tableColumn == tableView.tableColumns[0] {
+            cellIdentifier = CellIdentifiers.NameCell
+        }
+        
+        // 3
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = tableViewData[row]
+            return cell
+        }
+        return nil
     }
     
 }
